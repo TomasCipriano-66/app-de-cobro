7258e52b534354e0359ba06b73a6cc60be0f0495
TomasCipriano <d47173704@alumnos.itr.edu.ar>
2024-09-07 12:48:10 -0300

    paguina de compras

--- app.py
+++ app.py
@@ -3,47 +3,24 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_USER'] = 'u2jsmodvktmxthwk'
-app.config['MYSQL_PASSWORD'] = 'JTSZ6HR3AMgW3HDo7ykz'
-app.config['MYSQL_HOST'] = 'bwpeifpsxajflfti7mrf-mysql.services.clever-cloud.com' 
-app.config['MYSQL_DB'] = 'bwpeifpsxajflfti7mrf'
+app.config['MYSQL_USER'] = 'unywje4ax5puclwk'
+app.config['MYSQL_PASSWORD'] = 'Uz9KmlHRoqhzEMEQAuAl'
+app.config['MYSQL_HOST'] = 'bgbng8uouisbf7l4ajx0-mysql.services.clever-cloud.com'
+app.config['MYSQL_DB'] = 'bgbng8uouisbf7l4ajx0'
 mysql = MySQL(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    # Conectar a la base de datos y obtener productos
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT nombre, precio, stock, img FROM productos")
+    productos = cursor.fetchall()
+    cursor.close()
+    return render_template('home.html', productos=productos)
 
 @app.route('/carrito')
 def carrito():
     return render_template('carrito.html')
 
-
-# --------------SECCION ADMINS----------------
-
-#cargar tablas
-@app.route('/ADMIN')
-def admin():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM Inscripcion')
-    data1 = cur.fetchall()
-    cur.close()
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM Copa_renault')
-    data2 = cur.fetchall()
-    cur.close()
-    return render_template('ADMIN.html', Table1_inf = data1, Table2_inf = data2)
-
-#eliminar un equipo de la DB
-@app.route('/delete/<string:id>')
-def delete_team(id):
-    cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM Inscripcion WHERE id = %s', [id])
-    mysql.connection.commit()
-    cur.close()
-    return redirect(url_for('admin'))
-
-
-
-
 if __name__ == '__main__':
-    app.run(port=2500, debug=True)
+    app.run(debug=True)
